3053310d6447b2f5b854fbdbdd4ec73fa39e2556
aleksandr <alexander_sheykin@mail.ru>
2026-02-02 00:18:20 +0300

    feat: add argument parser and error handling
    - Implement argparse for CLI file path input
    - Add try/except block in main() for FileNotFoundError
    - Set default log file to './access.log'

--- python_01_nginx_access_log/main.py
+++ python_01_nginx_access_log/main.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python3
 
-import sys
+import argparse
 from collections import Counter
 from itertools import islice
 
 def main(file: str) -> dict:
-    with open(file, 'r') as f:
-        status = [i.split()[-4] for i in f.readlines()]
-    return dict(islice(Counter(status).items(), 3))
+    try:
+        with open(file, 'r') as f:
+            status = [i.split()[-4] for i in f.readlines()]
+        return dict(islice(Counter(status).items(), 3))
+    except FileNotFoundError:
+        print(f"Error: {file} not found")
+        return dict()
+
 
 if __name__ == '__main__':
-    file = sys.argv[1]
-    for key,value in main(file).items():
+
+    parser = argparse.ArgumentParser(description='Parse Nginx status codes')
+    parser.add_argument('file', nargs='?', default='./access.log', help='Log File Path')
+    args = parser.parse_args()
+
+    for key,value in main(args.file).items():
         print(f'{key}: {value}')
